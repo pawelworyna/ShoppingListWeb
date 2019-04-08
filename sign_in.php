ff518b28e01881127ecdbcc30c75507cee88b8a3
Pawel Woryna <pawel.woryna@gmail.com>
2019-04-08 19:33:27 +0200

    fix sign in

--- sign_in.php
+++ sign_in.php
@@ -1,33 +1,34 @@
 <?php
   require "configDB.php";
-  $checkNameUsr = $_POST['nameUsr']
-  $checkPswd = $_POST['password'];
 
-  $connection = mysqli($dbhost, $dbuser, $dbpass, $db);
+  $checkNameUsr = $_POST['nameUsr'];
+  $checkPswd = $_POST['passwordUsr'];
+
+  $connection = new mysqli($dbhost, $dbuser, $dbpass, $db);
 
   $query = "SELECT * FROM users";
 
   $result = mysqli_query($connection, $query);
 
-  $response = array()
+  $response = array();
 
     while($row = mysqli_fetch_assoc($result))
     {
         $user_id = $row['id'];
         $user_name = $row['name'];
-        $user_password = $row['password']
-        if($user_name == $checkNameUsr &&  $user_pswd == $checkPswd){
+        $user_password = $row['password'];
+        if($user_name == $checkNameUsr &&  $user_password == $checkPswd){
           $response['status'] = 1;
-          $response['usr_id'] = $user_id
+          $response['usr_id'] = $user_id;
           break;
         }
 
         $response['status'] = 0;
-        $response['usr_id'] = null
+        $response['usr_id'] = null;
 
     }
 
-    echo json_decode($response, true);
+    echo json_encode($response);
 
 
  ?>
